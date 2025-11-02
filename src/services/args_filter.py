a5ed7040c8a9565c26f06c601a8c9681b135c5cf
Konstantin Smage <kpsmage@mai.education>
2025-11-03 02:10:58 +0300

    Add options filter
    Add decorator for check available options

---
+++ src/services/args_filter.py
@@ -0,0 +1,21 @@
+import logging
+
+from src.common.config import logging_config
+
+logger = logging.getLogger(__name__)
+logging_config(level=logging.DEBUG)
+
+
+def options_filter(available_options: list = []):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+
+            for option in kwargs['options']:
+                if not (option in available_options):
+                    logger.error(f'Invalid option for {kwargs['cmd']}: {option}')
+                    raise ValueError(f'{kwargs['cmd']}: {option}: invalid option')
+
+            result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
--- src/services/console.py
+++ src/services/console.py
@@ -2,14 +2,18 @@ import logging
 from os import PathLike
 from pathlib import Path
 
+from src.services.args_filter import options_filter
+
 
 class Console:
     def __init__(self, logger: logging.Logger):
         self._logger = logger
 
+    @options_filter()
     def exit(self, cmd: str, options: list, args: list) -> None:
         self._logger.info(f'Interrupted by command: {cmd}')
         raise KeyboardInterrupt
 
+    @options_filter(['-l'])
     def ls(self, cmd: str, options: list, args: list):
         pass
