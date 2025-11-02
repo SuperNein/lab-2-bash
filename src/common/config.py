e686dec0e01f7e4f63e4464d9dbd3f1696f79a26
Konstantin Smage <kpsmage@mai.education>
2025-11-03 00:54:47 +0300

    Fix logging config
    Remake shell.log path

--- src/common/config.py
+++ src/common/config.py
@@ -1,9 +1,13 @@
 import logging
+from pathlib import Path
 
 def logging_config(level=logging.INFO):
+    current_file = Path(__file__).resolve()
+    filename = current_file.parent.parent.parent / 'shell.log'
+
     logging.basicConfig(
         level=level,
         datefmt="%Y-%m-%d %H:%M:%S",
         format="[%(asctime)s.%(msecs)03d] %(levelname)8s: %(message)s",
-        filename="../shell.log",
+        filename=filename,
     )
