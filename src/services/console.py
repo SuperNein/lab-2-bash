f9aad0a61e8128a23062933401507e9528168f34
Konstantin Smage <kpsmage@mai.education>
2025-11-03 21:42:05 +0300

    Add Console.ls()
    Add method

--- src/services/console.py
+++ src/services/console.py
@@ -16,6 +16,23 @@ class Console:
         self._logger.info(f'Interrupted by command: {cmd}')
         raise KeyboardInterrupt
 
-    @options_filter(['-l'])
-    def ls(self, cmd: str, options: list, args: list):
-        pass
+    @options_filter(available_options=['-l'])
+    @args_filter(args_num=[0, 1])
+    def ls(self, cmd: str, options: list, args: list) -> list[str]:
+        if args:
+            path = Path(args[0])
+            if not path.is_absolute():
+                path = (current_dir / path).resolve()
+        else:
+            path = current_dir
+        
+        if not path.exists():
+            self._logger.error(f'Folder not found: {path!r}')
+            raise FileNotFoundError(f'{cmd}: cannot access {path!r}: No such file or directory')
+        
+        if not path.is_dir():
+            self._logger.error(f'{path!r} is not a directory')
+            raise NotADirectoryError(f'{cmd}: cannot access {path!r}: Not a directory')
+
+        self._logger.info(f'Listing {path!r}')
+        return [entry.name for entry in path.iterdir()]
