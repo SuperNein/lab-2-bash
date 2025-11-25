11b8b72cfafb331c7a292408ac6139bd9c8df299
Konstantin Smage <kpsmage@mai.education>
2025-11-25 05:51:23 +0300

    fix: Fix rm
    - Fix moving files to .trash while removing

--- src/common/constants.py
+++ src/common/constants.py
@@ -1,4 +1,7 @@
+import os
 from pathlib import Path
 
 TRASH_DIR = Path(__file__).parent.parent.parent / ".trash"
+TRASH_DIR.mkdir(exist_ok=True)
+
 HISTORY_DIR = Path(__file__).parent.parent.parent / ".history"
